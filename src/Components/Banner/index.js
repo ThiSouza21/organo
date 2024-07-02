f72895bd7b17cfad4cc31728b422da8795853af9
ThiSouza21 <thiagovss21@gmail.com>
2024-07-02 09:03:03 -0300

    first commit

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <img
+      className="bannerOrganoHero"
+      src="/assets/images/banner.png"
+      alt="Banner Organo"
+    />
+  );
+};
+
+export default Banner;
